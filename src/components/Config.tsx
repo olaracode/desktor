022d3d2048c31082cbf5f1fef9897bb375adae14
Octavio Lara <octavioasapchi@gmail.com>
2024-05-28 16:32:18 -0400

    [🚧 ADD] config wrapper

--- src/components/Config.tsx
+++ src/components/Config.tsx
@@ -1,15 +1,20 @@
 "use client";
-import React from "react";
-import { invoke } from "@tauri-apps/api";
-
-async function getConfigs() {
-  const response = await invoke("get_configs");
-  console.log(response);
-}
-
+import React, { useState } from "react";
+import useBoundStore from "@/store";
+import { useRouter } from "next/navigation";
 const Config = () => {
-  getConfigs();
-  return <div>Config</div>;
+  const router = useRouter();
+  const { getFromFile, config } = useBoundStore();
+  React.useEffect(() => {
+    if (!config) {
+      getFromFile();
+    } else {
+      if (config.status == "UserSetup") router.push("/config/user");
+      if (config.status == "Setup") router.push("/config/setup");
+      if (config.status == "Complete") router.push("/dashboard");
+    }
+  }, [config]);
+  return null;
 };
 
 export default Config;
